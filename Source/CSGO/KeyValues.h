186083c1a7b1e2cf51a51eba41905309848f77a1
molotowKR <125775873+molotowKR@users.noreply.github.com>
2023-02-23 22:37:29 -0500

    Delete Source directory

--- Source/CSGO/KeyValues.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "../Memory.h"
-
-class KeyValues {
-public:
-    KeyValues* findKey(const Memory& memory, const char* keyName, bool create) noexcept;
-    void setString(const Memory& memory, const char* keyName, const char* value) noexcept;
-};
